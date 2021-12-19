8be9327c19539080ba903dcfcca783d722ae0602
Henrique-Rodrigues-Santana <henrique.rocdrigue@gmail.com>
2021-12-19 12:39:25 -0300

    Parte-1

---
+++ etapas.js
@@ -0,0 +1,57 @@
+let etapas =  [
+    {
+        titulo: 'Veriador',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 3811,
+                nume: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {
+                        url:'assets/img/3811.jpg', legenda:'Veriador', small:false
+                    }
+                ]
+            },
+            {
+                numero: 77222,
+                nume: 'Beltrano da Silva',
+                partido: 'DEF',
+                fotos:[
+                    {
+                        url:'assets/img/77222.jpg', legenda:'Veriador'
+                    }
+                ]
+            }
+
+        ]
+        
+
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero:99,
+                nome:'Ciclano',
+                partido: 'ABC',
+                vic:'Cic',
+                fotos:[
+                    {url:'99.jpg', legenda:'Prefeito'},
+                    {url:'99-2.jpg', legenda:'Vice-Prefeito'}
+                ]
+            },
+            {
+                numero:84,
+                nome:'Zulano',
+                partido: 'QWDSD',
+                vic:'Zul',
+                fotos:[
+                    {url:'84.jpg', legenda:'Prefeito'},
+                    {url:'84-2.jpg', legenda:'Vice-Prefeito'}
+                ]
+            }
+        ]
+    }
+]
---
+++ script.js
@@ -0,0 +1,71 @@
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3');
+
+
+let etapaAtual = 0;
+
+function comecarEtapa(){
+    let etapa = etapas[etapaAtual];
+
+    let numeroHtml = '';
+    let numero = '';
+
+    for(let i = 0; i < etapa.numeros;i++){
+        if(i === 0 ){
+            numeroHtml += '<div class="numero pisca"></div>';
+        }else {
+            numeroHtml += '<div class="numero"></div>';
+        }
+        
+    }
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.desplay = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+
+}
+
+function atualizaInterface(){
+    
+}
+
+
+function clicou(n){
+    let elnumero = document.querySelector('.numero.pisca');
+    if(elnumero !== null){
+        elnumero.innerHTML = n;
+        numero = '${numero}${n}';
+        /*removendo uma class da class pai */
+        elnumero.classList.remove('pisca');
+        /* retornando a class pisca a class pai  */
+        /*primeiro verificar se há um proximo elemento */
+        if( elnumero.nextElementSibling !== null){
+        elnumero.nextElementSibling.classList.add('pisca');
+        }else{
+            atualizaInterface();
+        }
+    }
+}
+
+function branco(){
+
+}
+
+function corrige(){
+
+}
+
+function confirmar(){
+
+}
+
+comecarEtapa();
+
