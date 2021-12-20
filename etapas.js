78708bbf1e544c3530ba3ec2cec44b4358df2722
Henrique-Rodrigues-Santana <henrique.rocdrigue@gmail.com>
2021-12-20 08:31:41 -0300

    Fim da Urna

--- etapas.js
+++ etapas.js
@@ -4,22 +4,22 @@ let etapas =  [
         numeros: 5,
         candidatos: [
             {
-                numero: 3811,
-                nume: 'Fulano de Tal',
+                numero: '38112',
+                nome: 'Fulano de Tal',
                 partido: 'ABC',
                 fotos:[
                     {
-                        url:'assets/img/3811.jpg', legenda:'Veriador', small:false
+                        url:'assets/img/C01.jpg', legenda:'Veriador', small:false
                     }
                 ]
             },
             {
-                numero: 77222,
+                numero: '77222',
                 nume: 'Beltrano da Silva',
                 partido: 'DEF',
                 fotos:[
                     {
-                        url:'assets/img/77222.jpg', legenda:'Veriador'
+                        url:'assets/img/C02.jpg', legenda:'Veriador'
                     }
                 ]
             }
@@ -33,23 +33,23 @@ let etapas =  [
         numeros: 2,
         candidatos: [
             {
-                numero:99,
+                numero:'99',
                 nome:'Ciclano',
                 partido: 'ABC',
                 vic:'Cic',
                 fotos:[
-                    {url:'99.jpg', legenda:'Prefeito'},
-                    {url:'99-2.jpg', legenda:'Vice-Prefeito'}
+                    {url:'assets/img/P01.jpg', legenda:'Prefeito'},
+                    {url:'assets/img/P02.jpg', legenda:'Vice-Prefeito'}
                 ]
             },
             {
-                numero:84,
+                numero:'84',
                 nome:'Zulano',
                 partido: 'QWDSD',
                 vic:'Zul',
                 fotos:[
-                    {url:'84.jpg', legenda:'Prefeito'},
-                    {url:'84-2.jpg', legenda:'Vice-Prefeito'}
+                    {url:'assets/img/V01.jpg', legenda:'Prefeito'},
+                    {url:'assets/img/V02.jpg', legenda:'Vice-Prefeito'}
                 ]
             }
         ]
--- script.js
+++ script.js
@@ -8,13 +8,20 @@ let numeros = document.querySelector('.d-1-3');
 
 
 let etapaAtual = 0;
+/*armazena os numeros da urna */
+let numero = '';
+
+let vbranco = false;
+
+let votoValido = false;
 
 function comecarEtapa(){
     let etapa = etapas[etapaAtual];
 
     let numeroHtml = '';
-    /*armazena os numeros da urna */
-    let numero = '';
+    numero = '';
+    vbranco = false;
+    
 
     for(let i = 0; i < etapa.numeros;i++){
         if(i === 0 ){
@@ -35,7 +42,34 @@ function comecarEtapa(){
 }
 
 function atualizaInterface(){
-    console.log(numeroOO);
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item)=>{
+        if(item.numero === numero){
+            return true;
+        }else{
+            return false;
+        }
+    })
+
+    if(candidato.length > 0){
+        candidato = candidato[0];
+        seuVotoPara.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome}</br>Partido: ${candidato.partido}`;
+        aviso.style.desplay = 'block';
+
+        let fotosHtml = '';
+        for(let i in candidato.fotos){
+            fotosHtml += `<div class="d-1-image"><img src="${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`;
+        }
+        lateral.innerHTML = fotosHtml;
+        
+
+    }else{
+        seuVotoPara.style.display = 'block';
+        descricao.innerHTML = '<div class="aviso--grande pisca">Voto Nulo</div>';
+        
+        
+    }
 }
 
 
@@ -43,7 +77,7 @@ function clicou(n){
     let elnumero = document.querySelector('.numero.pisca');
     if(elnumero !== null){
         elnumero.innerHTML = n;
-        numeroOO = '${numero}${n}';
+        numero = `${numero}${n}`;
 
         elnumero.classList.remove('pisca');
 
@@ -59,14 +93,45 @@ function clicou(n){
 }
 
 function branco(){
-
+   
+        numero = '';
+        vbranco = true;
+        seuVotoPara.style.display = 'block';
+        numeros.innerHTML = '';
+        descricao.innerHTML = '<div class="aviso--grande pisca">Voto Em Branco</div>';
+        lateral.innerHTML = '';
+        
+    
 }
 
 function corrige(){
-
+    return comecarEtapa();
 }
 
 function confirmar(){
+    let votoConfirmado = false;
+    let etapa = etapas[etapaAtual];
+    if(vbranco === true){
+        votoConfirmado = true;
+        
+    }else if(numero.length === etapa.numeros){
+        votoConfirmado = true;
+        
+    }
+
+    if(votoConfirmado){
+        etapaAtual++;
+        if(etapas[etapaAtual] !== undefined){
+            comecarEtapa();
+        }else{
+            seuVotoPara.style.display = 'none';
+            cargo.innerHTML = '<div class="aviso--grande2 pisca">FIM</div>';
+            descricao.innerHTML = '';
+            aviso.style.desplay = 'none';
+            lateral.innerHTML = '';
+            numeros.innerHTML = numeroHtml;
+        }
+    };
 
 }
 
